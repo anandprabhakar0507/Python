02661a79e1ff9ff3a1f3803c152d3135098f5be0
Anand prabhakar <anandprabhakar0507@gmail.com>
2021-10-14 16:24:38 +0530

    Update cmd_argparse.py

--- cmd/cmd_argparse.py
+++ cmd/cmd_argparse.py
@@ -1,9 +1,10 @@
 # better method then sys module
 import argparse as arg
 parser=arg.ArgumentParser()
-parser.add_argument('arg1',help='1st argument',type=int)
-parser.add_argument('arg2',help='2nd argument',type=int)
-args=parser.parse_args()
+parser.add_argument('arg1',help='Enter a number',type=float)
+parser.add_argument('arg2',help='Enter another',type=float)
+args=parser.parse_args() #it parses the arguments
 print('sum of these argment is: ',args.arg1+args.arg2)
+print(f'these were the argumnets you entered {args.arg1} and {args.arg2}') # get the arguments out
 # convert in dictionary by vars
 print('dictionary is: ',vars(args))
